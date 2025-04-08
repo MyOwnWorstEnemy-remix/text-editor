d3a7933d8ba78e743d87b20e3395a7063918baa1
HurricaneLily <jostin27022023@gmail.com>
2025-04-08 14:56:57 +0400

    Remove options

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const editor = document.querySelector('.editor');
 const templatesList = document.querySelector('.template__list');
 
 function counter () {
-    let count = 1;
+    let count = 2;
 
     return function () {
         count += 1;
@@ -33,6 +33,7 @@ const onInsertButtonClick = () => {
 insertButton.addEventListener('click', onInsertButtonClick);
 
 const addTemplateButton = document.querySelector('.template__button--add');
+const removeTemplateButton = document.querySelector('.template__button--remove');
 const templateInput = document.querySelector('.template__input');
 const templateNumber = counter();
 
@@ -63,3 +64,24 @@ addTemplateButton.addEventListener('click', () => {
     })
 })
 
+removeTemplateButton.addEventListener('click', () => {
+    const selected = document.querySelector('input[name="template"]:checked');
+    const item = selected.closest('li');
+    templatesList.removeChild(item);
+
+    const selectsList = document.querySelectorAll('select');
+    selectsList.forEach((select) => {
+        const optionsList = select.querySelectorAll('option');
+        const selectedOption = select.selectedOptions[0];
+        optionsList.forEach((option) => {
+            if(option.value === selected.value) {
+                if (selectedOption.value !== option.value) {
+                    select.removeChild(option);
+                } else {
+                    option.textContent = 'Error';
+                    option.value = 'error';
+                }
+            }
+        });
+    })
+})
